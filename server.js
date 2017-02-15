860c34e2315a75e80f08666adab7c16bc49dd8f8
umamage <umamage@gmail.com>
2017-02-15 13:02:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/article-one',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
-   res.send("Article two is requested and will be sent"); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function(req,res){
    res.send("Article three is requested and will be sent"); 
